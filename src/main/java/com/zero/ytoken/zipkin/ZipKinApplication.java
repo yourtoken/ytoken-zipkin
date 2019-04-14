5d35cf151718014afc2889d21393b85017387fef
Author.zero <1090863704@qq.com>
2019-04-14 11:44:08 +0800

    inti commit

---
+++ src/main/java/com/zero/ytoken/zipkin/ZipKinApplication.java
@@ -0,0 +1,21 @@
+package com.zero.ytoken.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+/**
+ * @classname: ZipKinApplication
+ * @description: TODO
+ * @author: Author.zero
+ * @create: 2019-04-14 11:27
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipKinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class,args);
+    }
+}
